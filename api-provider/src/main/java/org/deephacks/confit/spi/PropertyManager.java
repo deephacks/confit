1f31707f7e94f12a57968727138f19d54a526a8f
Kristoffer Sjogren <stoffe@gmail.com>
2013-08-03 16:32:01 +0200

    Documentation for PropertyManager

--- api-provider/src/main/java/org/deephacks/confit/spi/PropertyManager.java
+++ api-provider/src/main/java/org/deephacks/confit/spi/PropertyManager.java
@@ -7,6 +7,12 @@ import org.deephacks.confit.model.Schema;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Responsible for reading configuration from property files using an unspecified format.
+ *
+ * Configuration read from property files is used for bootstrap and fallback for configuration
+ * that do not exist in the BeanManager.
+ */
 public abstract class PropertyManager {
 
     /**
@@ -18,8 +24,21 @@ public abstract class PropertyManager {
         return Lookup.get().lookup(PropertyManager.class);
     }
 
+    /**
+     * Get a simple string property.
+     *
+     * @param name name of the property.
+     * @return value if it exist.
+     */
     public abstract Optional<String> get(String name);
 
+    /**
+     * List all beans of a specific Schema.
+     *
+     * @param schema schema to list
+     * @param schemas all schemas currently available
+     * @return all beans found
+     */
     public abstract List<Bean> list(Schema schema, Map<String, Schema> schemas);
 
 }
